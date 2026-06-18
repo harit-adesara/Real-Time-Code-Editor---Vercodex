61eb088ebfdb0c0651af03f6691671cacd97f7d2
Harit-01 <harit13soni@gmail.com>
2026-06-18 09:25:24 +0530

    scroll design fix

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -7,21 +7,28 @@ import Footer from "../components/Footer.jsx";
 
 const Dashboard = () => {
   return (
-    <div className="bg-[rgb(2,6,23)] min-h-screen flex">
+    <div className="bg-[rgb(2,6,23)] h-screen flex overflow-hidden">
       {/* Sidebar */}
-      <Sidebar />
+      <div className="h-full shrink-0">
+        <Sidebar />
+      </div>
 
-      {/* Right Side */}
-      <div className="flex flex-col flex-1 min-h-screen">
-        <Header />
+      {/* Right Section */}
+      <div className="flex flex-col flex-1 h-full">
+        {/* Header */}
+        <div className="shrink-0">
+          <Header />
+        </div>
 
-        {/* Main Content */}
-        <main className="flex-1 p-4 md:p-6 overflow-y-auto">
+        {/* Scrollable Content */}
+        <main className="flex-1 overflow-y-auto p-4 md:p-6">
           <Outlet />
         </main>
 
         {/* Footer */}
-        <Footer />
+        <div className="shrink-0">
+          <Footer />
+        </div>
       </div>
     </div>
   );
